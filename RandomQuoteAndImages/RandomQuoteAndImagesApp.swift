ec02ce45ca843b9f22a82e84d799f00f92f172b6
Mohammad Azam <azamsharp@gmail.com>
2021-07-14 21:56:49 -0500

    Initial Commit

---
+++ RandomQuoteAndImages/RandomQuoteAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImagesApp.swift
+//  RandomQuoteAndImages
+//
+//  Created by Mohammad Azam on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
